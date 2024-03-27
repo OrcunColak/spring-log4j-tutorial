42af94e738b84b0dae5d4bd3c97d1fe6c0e74735
Orçun Çolak <orcun.colak@hazelcast.com>
2024-03-27 13:16:15 +0300

    log for package example

--- src/main/java/com/colak/springlog4jtutorial/SpringLog4jTutorialApplication.java
+++ src/main/java/com/colak/springlog4jtutorial/SpringLog4jTutorialApplication.java
@@ -1,13 +1,21 @@
 package com.colak.springlog4jtutorial;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringLog4jTutorialApplication {
+@Slf4j
+public class SpringLog4jTutorialApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringLog4jTutorialApplication.class, args);
-	}
+    public static void main(String[] args) {
 
+        SpringApplication.run(SpringLog4jTutorialApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        log.debug("Debug message");
+    }
 }
