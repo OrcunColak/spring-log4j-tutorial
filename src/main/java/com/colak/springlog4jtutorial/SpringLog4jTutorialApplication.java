84321377c53775b9b24cd81232d41bf034a68766
Orçun Çolak <orcun.colak@hazelcast.com>
2024-03-27 13:26:53 +0300

    log for package example

--- src/main/java/com/colak/springlog4jtutorial/SpringLog4jTutorialApplication.java
+++ src/main/java/com/colak/springlog4jtutorial/SpringLog4jTutorialApplication.java
@@ -1,6 +1,8 @@
 package com.colak.springlog4jtutorial;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
